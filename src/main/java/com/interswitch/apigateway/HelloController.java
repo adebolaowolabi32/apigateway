b7eef5feb23f2802273f37466ce8c45ada49fb81
adesegun.adeyemo <adesegun.adeyemo@interswitchgroup.com>
2019-01-23 12:23:29 +0100

    initial commit

---
+++ src/main/java/com/interswitch/apigateway/HelloController.java
@@ -0,0 +1,26 @@
+package com.interswitch.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static net.logstash.logback.argument.StructuredArguments.value;
+
+@RestController
+public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Mono<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        var greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+        logger.info("Message", value("greeting", greeting));
+        return Mono.just(greeting);
+    }
+}
