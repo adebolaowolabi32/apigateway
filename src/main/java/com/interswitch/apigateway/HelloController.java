90f333400db71621031bd4c0a4c8073a57053347
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-01-25 18:03:58 +0100

    commented properties out from application.properties

--- src/main/java/com/interswitch/apigateway/HelloController.java
+++ src/main/java/com/interswitch/apigateway/HelloController.java
@@ -3,6 +3,8 @@ package com.interswitch.apigateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import static net.logstash.logback.argument.StructuredArguments.value;
 
 @RestController
+@ConfigurationProperties
 public class HelloController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @Value("${spring.application.name}")
+    @Value("${spring.redis.sentinel.master}")
     private String greetingmessage;
 
     @GetMapping("/greeting")
