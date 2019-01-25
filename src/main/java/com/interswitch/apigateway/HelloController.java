0a53098b216157c3fb8a6882e9f8584203b4275e
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-01-25 15:02:36 +0100

    commented properties out from application.properties

--- src/main/java/com/interswitch/apigateway/HelloController.java
+++ src/main/java/com/interswitch/apigateway/HelloController.java
@@ -2,6 +2,7 @@ package com.interswitch.apigateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,13 @@ public class HelloController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Value("${spring.application.name}")
+    private String greetingmessage;
+
     @GetMapping("/greeting")
     public Mono<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        var greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
-        logger.info("Message", value("greeting", greeting));
+        var greeting = new Greeting(counter.incrementAndGet(), greetingmessage);
+        logger.info("Message", greeting);
         return Mono.just(greeting);
     }
 }
