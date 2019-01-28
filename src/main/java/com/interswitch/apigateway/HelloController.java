369ae90f3d1d041312619d9d877492e27cf39f79
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-01-28 08:48:57 +0100

    removed spring data for mongodb

--- src/main/java/com/interswitch/apigateway/HelloController.java
+++ src/main/java/com/interswitch/apigateway/HelloController.java
@@ -2,9 +2,6 @@ package com.interswitch.apigateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +17,11 @@ public class HelloController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @Value("${spring.redis.sentinel.master}")
-    private String greetingmessage;
 
     @GetMapping("/greeting")
     public Mono<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        var greeting = new Greeting(counter.incrementAndGet(), greetingmessage);
-        logger.info("Message", greeting);
+        var greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+        logger.info("Message", value("greeting", greeting));
         return Mono.just(greeting);
     }
 }
--- src/main/java/com/interswitch/apigateway/repository/RouteDefinitionRepositoryMongo.java
+++ src/main/java/com/interswitch/apigateway/repository/RouteDefinitionRepositoryMongo.java
@@ -2,6 +2,8 @@ package com.interswitch.apigateway.repository;
 
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RouteDefinitionRepositoryMongo extends ReactiveMongoRepository<RouteDefinition, String> {
 }
