3e484dc52889745c5826460925e738fd6350eecd
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-02-20 13:35:59 +0100

    updated enablecors filter to prevent duplicate cors headers, updated unit tests

--- src/main/java/com/interswitch/apigateway/filter/EnableCorsFilter.java
+++ src/main/java/com/interswitch/apigateway/filter/EnableCorsFilter.java
@@ -1,6 +1,8 @@
 package com.interswitch.apigateway.filter;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -8,13 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
-public class EnableCorsFilter implements WebFilter, Ordered {
+public class EnableCorsFilter implements GlobalFilter, Ordered {
 
     private static final List<String> ALLOWED_HEADERS = Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization");
     private static final List<HttpMethod> ALLOWED_METHODS = Arrays.asList(HttpMethod.GET, HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.OPTIONS);
@@ -26,20 +26,21 @@ public class EnableCorsFilter implements WebFilter, Ordered {
     private static final Boolean ALLOW_CREDENTIALS = true;
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        ServerHttpResponse response = exchange.getResponse();
-        HttpHeaders headers = response.getHeaders();
-        headers.setAccessControlAllowOrigin(ALLOWED_ORIGIN);
-        headers.setAccessControlAllowHeaders(ALLOWED_HEADERS);
-        headers.setAccessControlAllowMethods(ALLOWED_METHODS);
-        headers.setAccessControlAllowCredentials(ALLOW_CREDENTIALS);
-        headers.setAccessControlMaxAge(MAX_AGE);
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            HttpHeaders headers = response.getHeaders();
+            headers.setAccessControlAllowOrigin(ALLOWED_ORIGIN);
+            headers.setAccessControlAllowHeaders(ALLOWED_HEADERS);
+            headers.setAccessControlAllowMethods(ALLOWED_METHODS);
+            headers.setAccessControlAllowCredentials(ALLOW_CREDENTIALS);
+            headers.setAccessControlMaxAge(MAX_AGE);
 
-        if (request.getMethod() == HttpMethod.OPTIONS) {
-            response.setStatusCode(HttpStatus.OK);
-        }
-        return chain.filter(exchange);
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+            }
+        }));
 
     }
 
