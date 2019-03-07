0a44a1bed275de1c0f525e99022574dda04d540d
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-03-07 16:30:08 +0100

    checks during tests

--- src/main/java/com/interswitch/apigateway/filter/AccessControlFilter.java
+++ src/main/java/com/interswitch/apigateway/filter/AccessControlFilter.java
@@ -1,86 +1,76 @@
 package com.interswitch.apigateway.filter;
 
-import io.jsonwebtoken.Claims;
+import com.interswitch.apigateway.repository.ClientResourcesRepository;
 import org.jose4j.jwa.AlgorithmConstraints;
-import org.jose4j.jwk.RsaJsonWebKey;
-import org.jose4j.jwk.RsaJwkGenerator;
 import org.jose4j.jws.AlgorithmIdentifiers;
-import org.jose4j.jwt.JwtClaims;
 import org.jose4j.jwt.MalformedClaimException;
 import org.jose4j.jwt.consumer.InvalidJwtException;
 import org.jose4j.jwt.consumer.JwtConsumer;
 import org.jose4j.jwt.consumer.JwtConsumerBuilder;
-import org.jose4j.jwt.consumer.JwtContext;
-import org.jose4j.keys.RsaKeyUtil;
-import org.jose4j.lang.JoseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import java.util.List;
+
 
 public class AccessControlFilter implements GlobalFilter, Ordered  {
     @Value("${spring.security.oauth2.resourceserver.jwt.keyValue}")
     private String key;
-    public  AccessControlFilter() {
 
-    }
-    RsaJsonWebKey rsaJsonWebKey;
+    private ClientResourcesRepository repository;
 
-    {
-        try {
-            rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
-        } catch (JoseException e) {
-            e.printStackTrace();
-        }
+    public  AccessControlFilter(ClientResourcesRepository repository) {
+        this.repository=repository;
+
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
-//        String accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        String client_id = "";
         String accessToken = exchange.getRequest().getHeaders().get("Authorization").toString();
-        accessToken= accessToken.replace(accessToken.substring(accessToken.indexOf("B") - 1, accessToken.indexOf(" ") + 1), "");
+        accessToken = accessToken.replace(accessToken.substring(accessToken.indexOf("B") - 1, accessToken.indexOf(" ") + 1), "");
+        String resourceId = (exchange.getRequest().getMethod().toString()) + (exchange.getRequest().getPath().toString());
+
         JwtConsumer jwtConsumer = new JwtConsumerBuilder()
                 .setRequireExpirationTime() // the JWT must have an expiration time
-                .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
-                .setRequireSubject() // the JWT must have a subject claim
-                .setExpectedIssuer("Issuer") // whom the JWT needs to have been issued by
-                .setExpectedAudience("Audience") // to whom the JWT is intended for
+                .setAllowedClockSkewInSeconds(30)// allow some leeway in validating time based claims to account for clock skew
+                .setExpectedAudience("isw-core")
+                .setDisableRequireSignature()
                 .setSkipSignatureVerification()
                 .setJwsAlgorithmConstraints( // only allow the expected signature algorithm(s) in the given context
                         new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, // which is only RS256 here
                                 AlgorithmIdentifiers.RSA_USING_SHA256))
                 .build();
-        try
-        {
-            JwtClaims jwtClaims = jwtConsumer.processToClaims(accessToken);
-            System.out.println("JWT validation succeeded! " + jwtClaims);
-        }
-        catch (InvalidJwtException e) {
-            JwtClaims claims = e.getJwtContext().getJwtClaims();
+
+        try {
+            client_id = jwtConsumer.processToClaims(accessToken).getStringClaimValue("client_id");
+        } catch (MalformedClaimException e) {
+            e.printStackTrace();
+        } catch (InvalidJwtException e) {
+            e.printStackTrace();
             try {
-                String client_id = claims.getStringClaimValue("client_id");
+                client_id = e.getJwtContext().getJwtClaims().getStringClaimValue("client_id");
             } catch (MalformedClaimException e1) {
                 e1.printStackTrace();
             }
-            System.out.println("Invalid JWT! " + e);
         }
-        String path = exchange.getRequest().getPath().toString();
-        String method = exchange.getRequest().getMethod().toString();
-        String clientId = exchange.getPrincipal().toString();
-        String resourceId = method+path;
-        return chain.filter(exchange);
-
+        return repository.findByClientId(client_id)
+                .flatMap(item -> {
+                    List resourceIds = item.getResourceIds();
+                    if (resourceIds.contains(resourceId)) {
+                        return chain.filter(exchange.mutate().build());
+                    } else {
+                        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN,"You are not allowed to use this resource"));
+                    }
+                })
+//                .doOnSuccess(c -> chain.filter(exchange))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "You do not have access to this resource")));
     }
 
     @Override
--- src/test/java/com/interswitch/apigateway/filter/AccessControlFliterTests.java
+++ src/test/java/com/interswitch/apigateway/filter/AccessControlFliterTests.java
@@ -1,4 +1,80 @@
 package com.interswitch.apigateway.filter;
 
+import com.interswitch.apigateway.config.CacheConfig;
+import com.interswitch.apigateway.controller.ClientResourcesController;
+import com.interswitch.apigateway.model.ClientResources;
+import com.interswitch.apigateway.repository.ClientResourcesRepository;
+import com.interswitch.apigateway.repository.MongoClientResourcesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ActiveProfiles("dev")
+@ContextConfiguration(classes = {MongoClientResourcesRepository.class,ClientResourcesRepository.class, AccessControlFilter.class})
+@Import(CacheConfig.class)
 public class AccessControlFliterTests {
+
+    @Autowired
+    private ClientResourcesRepository repository;
+    @MockBean
+    private MongoClientResourcesRepository mongo;
+
+    private AccessControlFilter filter;
+    private GatewayFilterChain filterChain;
+    private ArgumentCaptor<ServerWebExchange> captor;
+    private List testresourceIds = new ArrayList();
+    private ClientResources resource;
+    private String  clientId = "testclientid";
+
+    @BeforeEach
+    public void setup() {
+        filter = new AccessControlFilter(repository);
+        filterChain = mock(GatewayFilterChain.class);
+        captor = ArgumentCaptor.forClass(ServerWebExchange.class);
+        testresourceIds.add("passport/oauth/token");
+        testresourceIds.add("passport/oauth/authorize");
+        resource = new ClientResources("id",clientId,testresourceIds);
+    }
+
+    @Test
+    public void testAccessControl (){
+        when(repository.findByClientId(resource.getClientId())).thenReturn(Mono.empty());
+        MockServerHttpRequest request = MockServerHttpRequest
+                .options("http://localhost")
+                .header("Authorization","Bearer eyJhbGciOiJSUzI1NiJ9.eyJsYXN0TmFtZSI6IlRlc3RlciIsIm1lcmNoYW50X2NvZGUiOiJRVEVMTCIsInByb2R1Y3Rpb25fcGF5bWVudF9jb2RlIjoiMDUxNDYzNTEyNTY3MCIsImZpcnN0TG9naW4iOmZhbHNlLCJ1c2VyX25hbWUiOiJpc3d0ZXN0ZXIyQHlhaG9vLmNvbSIsInJlcXVlc3Rvcl9pZCI6IjAwMTYxMDkwOTA0IiwibW9iaWxlTm8iOiIyMzQ4MDU2NzMxNTc2IiwicGF5YWJsZV9pZCI6IjExNCIsImNsaWVudF9pZCI6IklLSUEzNDRCODkwMDk3MDAxNjQ3RUVEQjYwMjI2QTU4NTBBRTc1QzdDRDE5IiwiZmlyc3ROYW1lIjoiVGVzdGVycyIsImVtYWlsVmVyaWZpZWQiOnRydWUsImF1ZCI6WyJjYWVzYXIiLCJmaW5nZXJwcmludC1hcGkiLCJpbmNvZ25pdG8iLCJpc3ctY29sbGVjdGlvbnMiLCJpc3ctY29yZSIsImlzdy1pbnN0aXR1dGlvbiIsImlzdy1wYXltZW50Z2F0ZXdheSIsInBhc3Nwb3J0IiwicHJvamVjdC14LW1lcmNoYW50IiwidmF1bHQiXSwic2NvcGUiOlsicHJvZmlsZSJdLCJleHAiOjE1NTIwMDQ1MTEsIm1vYmlsZU5vVmVyaWZpZWQiOnRydWUsImp0aSI6ImU1MWYzZTE4LTFmZDMtNGYxZS1iOTg1LTFjZDI4MDMzYWExNiIsImVtYWlsIjoiaXN3dGVzdGVyMkB5YWhvby5jb20iLCJwYXNzcG9ydElkIjoiRTI1MUYwRTktN0JDRi00Q0FFLThFOEItNTZERjI1RUQ4NUQwIiwicGF5bWVudF9jb2RlIjoiMDUxNDYzNTEyNTY3MCJ9.OIcZkMfYoR1eq23nbMrgKHilCuSO3TyyGuUazcyIt7iJXpfLPjU3Y9byHcmz3nMmR7t3x3crztWDBKJUIuua-hyT36MBCzbpeXQmOswl1INWPGoEat_f5IMMXCGdU6W50XhFy4n4s4IAlrAQqGQrHiW7fHrFFrJeu3AVHl8utr-g560W2PlDyDGUss3aOt-plduXY6IFb6gTfoLsm2ZCGIR8s2lBmeSooSNDrw7ak368F2scbUhaFaHA4pDEas05AXO6dWh_qI9G_kJ40Loj_jCnacEdenVpiWJLyO2wBmQqPaMKW5MW_NOLgGEcXR-eRlN4n4NCtb2onE1y-DyQMQ")
+                .build();
+        assertAuthorizationHeader(request);
+    }
+
+
+    @Test
+    public ServerWebExchange assertAuthorizationHeader(MockServerHttpRequest request) {
+        ServerWebExchange exchange = MockServerWebExchange.from(request);
+        when(filterChain.filter(captor.capture())).thenReturn(Mono.empty());
+        filter.filter(exchange, filterChain).block();
+        ServerWebExchange webExchange = captor.getValue();
+
+        assertThat(webExchange.getResponse().getHeaders().containsKey("Authorization"));
+        return webExchange;
+    }
 }
