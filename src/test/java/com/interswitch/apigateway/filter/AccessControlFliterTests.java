bb45b6e16abdeb80dcd80055d6e1a56dacf2b7e1
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-03-12 17:19:13 +0100

    changed implementation

--- src/test/java/com/interswitch/apigateway/filter/AccessControlFliterTests.java
+++ src/test/java/com/interswitch/apigateway/filter/AccessControlFliterTests.java
@@ -1,6 +1,7 @@
 package com.interswitch.apigateway.filter;
 
 import com.interswitch.apigateway.config.CacheConfig;
+import com.interswitch.apigateway.model.Client;
 import com.interswitch.apigateway.repository.ClientCacheRepository;
 import com.interswitch.apigateway.repository.ClientMongoRepository;
 import com.nimbusds.jose.*;
@@ -8,7 +9,6 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,9 +24,7 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.text.ParseException;
-import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -38,7 +36,7 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.G
 @ContextConfiguration(classes = {CacheConfig.class,ClientCacheRepository.class, ClientMongoRepository.class, AccessControlFilter.class})
 public class AccessControlFliterTests {
 
-    @Autowired
+    @MockBean
     private ClientCacheRepository repository;
     @MockBean
     private ClientMongoRepository mongo;
@@ -46,6 +44,9 @@ public class AccessControlFliterTests {
     private GlobalFilter filter;
     private GatewayFilterChain filterChain  ;
     private String accessToken = "";
+    private String client_id = "IKIA344B890097001647EEDB60226A5850AE75C7CD19";
+    List testresourceIds = new ArrayList();
+    List origin=null;
 
     @BeforeEach
     public void setup() throws JOSEException, ParseException {
@@ -53,7 +54,7 @@ public class AccessControlFliterTests {
                 .expirationTime(new Date(new Date().getTime()+1000*60^10))
                 .notBeforeTime(new Date())
                 .audience("isw-core")
-                .claim("client_id", "IKIA344B890097001647EEDB60226A5850AE75C7CD19")
+                .claim("client_id", client_id)
                 .jwtID(UUID.randomUUID().toString())
                 .build();
         JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
@@ -61,11 +62,11 @@ public class AccessControlFliterTests {
         JWSObject jws = new JWSObject(jwsHeader,payload);
         jws.sign(new MACSigner("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"));
         accessToken = "Bearer " + jws.serialize();
-
         filter = new AccessControlFilter(repository);
         filterChain = mock(GatewayFilterChain.class);
-
+        testresourceIds.add("testid");
     }
+
 
     @Test
     public void testAccessControl (){
@@ -79,6 +80,8 @@ public class AccessControlFliterTests {
 
     @Test
     private void assertAuthorizationHeader(MockServerHttpRequest request) {
+        Client client = new Client("testclient",client_id,origin,testresourceIds);
+        when(repository.findByClientId(client_id)).thenReturn(Mono.just(client));
         Route value = Route.async().id("testid").uri(request.getURI()).order(0)
                 .predicate(swe -> true).build();
         ServerWebExchange exchange = MockServerWebExchange.from(request);
