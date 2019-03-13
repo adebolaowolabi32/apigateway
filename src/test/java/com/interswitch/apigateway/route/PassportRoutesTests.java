e8c012b13a4942a2a0d3439506a599b3063d5957
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-03-13 13:18:09 +0100

    updated tests

--- src/test/java/com/interswitch/apigateway/route/PassportRoutesTests.java
+++ src/test/java/com/interswitch/apigateway/route/PassportRoutesTests.java
@@ -1,26 +1,28 @@
 package com.interswitch.apigateway.route;
 
-import com.interswitch.apigateway.config.RouteConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+
 import java.nio.charset.Charset;
 import java.util.Map;
+
 import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("dev")
-@Import(RouteConfig.class)
+
 public class PassportRoutesTests {
     MultiValueMap formData = new LinkedMultiValueMap();
+
     @Autowired
     private WebTestClient webClient;
+
     private String clientId = "IKIAC4420D13ABE57D778FB7263A17D13B60A8AE4135";
     private String clientSecret = "secret";
 
