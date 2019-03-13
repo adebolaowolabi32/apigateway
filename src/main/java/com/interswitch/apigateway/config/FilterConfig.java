4a9b385a90a61df521c9dbb8db96196b20e240c5
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-03-13 11:39:21 +0100

    updated filter config

--- src/main/java/com/interswitch/apigateway/config/FilterConfig.java
+++ src/main/java/com/interswitch/apigateway/config/FilterConfig.java
@@ -1,26 +1,5 @@
 package com.interswitch.apigateway.config;
 
-import com.interswitch.apigateway.filter.AccessControlFilter;
-import com.interswitch.apigateway.filter.CorsFilter;
-import com.interswitch.apigateway.filter.RouteIdFilter;
-import com.interswitch.apigateway.repository.ClientCacheRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class FilterConfig {
-    @Bean
-    public CorsFilter corsFilter(){
-        return new CorsFilter();
-    }
 
-    @Bean
-    public RouteIdFilter routeIdFilter(){
-        return new RouteIdFilter();
-    }
-
-    @Bean
-    public AccessControlFilter accessControlFilter(ClientCacheRepository cacheRepository){
-        return new AccessControlFilter(cacheRepository);
-    }
 }
