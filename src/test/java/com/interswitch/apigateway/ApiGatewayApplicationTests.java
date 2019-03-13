c4960c3013167454ff35b4331e3af3babfdd30d6
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-03-13 15:16:17 +0100

    fixed zipkin dependency issue

--- src/test/java/com/interswitch/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/interswitch/apigateway/ApiGatewayApplicationTests.java
@@ -1,23 +1,15 @@
 package com.interswitch.apigateway;
 
-import com.interswitch.apigateway.model.Client;
-import com.interswitch.apigateway.repository.ClientMongoRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-@WebFluxTest
+
+@SpringBootTest
 @ActiveProfiles("dev")
-@ContextConfiguration(classes = {ClientMongoRepository.class})
 
 public class ApiGatewayApplicationTests {
-
-	@MockBean
-	ClientMongoRepository clientMongoRepository;
 
 	@Test
 	public void contextLoads() {
