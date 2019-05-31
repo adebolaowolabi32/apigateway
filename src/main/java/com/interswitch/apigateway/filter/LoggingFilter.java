86a3c2f81b21401729ad69cc5c3120bd02ed2ebf
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-05-31 11:06:37 +0100

    Added the downstream service URI to the logs

--- src/main/java/com/interswitch/apigateway/filter/LoggingFilter.java
+++ src/main/java/com/interswitch/apigateway/filter/LoggingFilter.java
@@ -4,10 +4,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.sleuth.instrument.web.TraceWebFilter;
 import org.springframework.core.Ordered;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
 
 public class LoggingFilter implements GlobalFilter, Ordered {
     protected static final String TRACE_REQUEST_ATTR = TraceWebFilter.class.getName()
@@ -22,7 +25,9 @@ public class LoggingFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String traceId = exchange.getAttribute(TRACE_REQUEST_ATTR).toString();
-        log.trace("Incoming Request: "+exchange.getRequest().getId()+ " TraceId: "+traceId+" Path: "+exchange.getRequest().getPath()+
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        String originalUri=route.getUri().toString();
+        log.trace("Incoming Request: "+exchange.getRequest().getId()+" TraceId: "+traceId+"To: "+originalUri+" Path: "+exchange.getRequest().getPath()+
                         " Method: "+exchange.getRequest().getMethodValue()+
                 " Headers: "+exchange.getRequest().getHeaders().values());
         return chain.filter(exchange);
