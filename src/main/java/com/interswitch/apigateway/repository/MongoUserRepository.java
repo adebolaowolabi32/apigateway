8c52095ad0d5f56bfdf991e762a574880931af3d
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-06-17 03:52:47 +0100

    added method to user repository

--- src/main/java/com/interswitch/apigateway/repository/MongoUserRepository.java
+++ src/main/java/com/interswitch/apigateway/repository/MongoUserRepository.java
@@ -3,7 +3,9 @@ package com.interswitch.apigateway.repository;
 import com.interswitch.apigateway.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface MongoUserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByUsername(String username);
 }
