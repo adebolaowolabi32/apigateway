7961e24f7f161ad3b377ad32672d04dd0ec4c064
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-06-28 16:18:23 +0100

    made changes to audience filter

--- src/main/java/com/interswitch/apigateway/filter/AudienceFilter.java
+++ src/main/java/com/interswitch/apigateway/filter/AudienceFilter.java
@@ -16,16 +16,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AudienceFilter implements WebFilter, Ordered {
+    private static String PassportRoute= "/passport/oauth/token";
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        List<String> audience = GetAudienceFromBearerToken(exchange.getRequest().getHeaders());
-        if(audience.contains("api-gateway"))
+        JWT token = DecodeBearerToken(exchange.getRequest().getHeaders());
+        if(token!=null) {
+            String exchangePath = exchange.getRequest().getPath().toString();
+            List<String> audience = GetAudience(token);
+            String environment = GetEnvironment(token);
+            if (audience.contains("api-gateway") & environment == "TEST" & exchangePath == PassportRoute)
+                return chain.filter(exchange);
+            return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have sufficient rights to this resource"));
+        }
+        else{
             return chain.filter(exchange);
-        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have sufficient rights to this resource"));
+        }
     }
 
-    public List<String> GetAudienceFromBearerToken(HttpHeaders headers) {
-        List<String> audience = new ArrayList<>();
+    public JWT DecodeBearerToken(HttpHeaders headers) {
+        JWT jwtToken= null;
         if (headers.containsKey(HttpHeaders.AUTHORIZATION)) {
             List<String> accesstokens = headers.get(HttpHeaders.AUTHORIZATION);
             if (accesstokens != null && !accesstokens.isEmpty()) {
@@ -34,9 +43,7 @@ public class AudienceFilter implements WebFilter, Ordered {
                     accesstoken = accesstoken.replaceFirst("Bearer ", "");
                     if (!accesstoken.isEmpty()) {
                         try {
-                            JWT jwtToken = JWTParser.parse(accesstoken);
-                            Object aud = jwtToken.getJWTClaimsSet().getClaim("aud");
-                            if(aud != null) audience = (List<String>)aud;
+                            jwtToken = JWTParser.parse(accesstoken);
                         } catch (ParseException e) {
                             Mono.error(e).log();
                         }
@@ -44,7 +51,34 @@ public class AudienceFilter implements WebFilter, Ordered {
                 }
             }
         }
+        return jwtToken ;
+    }
+    public List<String> GetAudience(JWT jwtToken){
+        List<String> audience = new ArrayList<>();
+        try {
+            Object aud = jwtToken.getJWTClaimsSet().getClaim("aud");
+            if (aud != null) audience = (List<String>) aud;
+        }
+        catch (ParseException e){
+            Mono.error(e).log();
+        }
         return audience;
+
+    }
+
+    public String GetEnvironment(JWT jwtToken){
+        String environment = "";
+        try {
+            Object env = jwtToken.getJWTClaimsSet().getClaim("env");
+            if (env != null) environment = env.toString();
+            return environment;
+        }
+        catch (ParseException e){
+            Mono.error(e).log();
+        }
+        return environment;
+
+
     }
 
     @Override
