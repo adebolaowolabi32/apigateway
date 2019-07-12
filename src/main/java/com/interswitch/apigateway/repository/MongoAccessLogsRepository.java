72dc09ce1e00d8d7e4da86cf9a7757516dc79635
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-07-12 17:38:29 +0100

    updated access logs

--- src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
+++ src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
@@ -1,15 +1,8 @@
 package com.interswitch.apigateway.repository;
 
 import com.interswitch.apigateway.model.AccessLogs;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-
 
 @Repository
-public interface MongoAccessLogsRepository extends ReactiveMongoRepository<AccessLogs, String> {
-    @Query("{id: { $exists: true}}")
-    Flux<AccessLogs> findAll(Pageable pageable);
-}
+public interface MongoAccessLogsRepository extends ReactiveMongoRepository<AccessLogs, String> {}
