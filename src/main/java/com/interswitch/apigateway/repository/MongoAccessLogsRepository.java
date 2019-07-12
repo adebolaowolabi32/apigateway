3be9a97edc2e2aa2ac4faf95424543c4d4d46ba2
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-07-12 17:39:57 +0100

    updated access logs

--- src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
+++ src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MongoAccessLogsRepository extends ReactiveMongoRepository<AccessLogs, String> {}
+public interface MongoAccessLogsRepository extends ReactiveMongoRepository<AccessLogs, String> {
+}
