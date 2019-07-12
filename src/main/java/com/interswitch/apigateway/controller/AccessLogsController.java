28bf8ba641fb09c1eab87f0265230b4dc1328450
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-07-12 10:29:47 +0100

    updated audit impl

--- src/main/java/com/interswitch/apigateway/controller/AccessLogsController.java
+++ src/main/java/com/interswitch/apigateway/controller/AccessLogsController.java
@@ -3,10 +3,13 @@ package com.interswitch.apigateway.controller;
 
 import com.interswitch.apigateway.model.AccessLogs;
 import com.interswitch.apigateway.repository.MongoAccessLogsRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/audit")
@@ -17,8 +20,8 @@ public class AccessLogsController {
         this.mongoAccessLogsRepository = mongoAccessLogsRepository;
     }
 
-    @GetMapping(produces = "application/json")
-    private Flux<AccessLogs> getAll() {
+    @GetMapping(value= "/{page}", produces = "application/json")
+    private Page<AccessLogs> getAll(@PathVariable int page) {
         return mongoAccessLogsRepository.findAll();
     }
 }
--- src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
+++ src/main/java/com/interswitch/apigateway/repository/MongoAccessLogsRepository.java
@@ -1,9 +1,13 @@
 package com.interswitch.apigateway.repository;
 
 import com.interswitch.apigateway.model.AccessLogs;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
 public interface MongoAccessLogsRepository extends ReactiveMongoRepository<AccessLogs, String> {
+    Page<AccessLogs> findAll(Pageable pageable);
 }
