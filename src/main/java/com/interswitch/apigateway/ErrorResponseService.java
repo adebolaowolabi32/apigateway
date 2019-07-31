f9467665f50039ff017ab737ce211b2aedf0e579
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-07-31 13:39:23 +0100

    added webserver input error exception

--- src/main/java/com/interswitch/apigateway/ErrorResponseService.java
+++ src/main/java/com/interswitch/apigateway/ErrorResponseService.java
@@ -1,18 +1,23 @@
 package com.interswitch.apigateway;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.interswitch.apigateway.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import net.logstash.logback.encoder.org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.support.NotFoundException;
 import org.springframework.core.codec.DecodingException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.mongodb.CannotGetMongoDbConnectionException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.ServerWebInputException;
 
+import javax.net.ssl.SSLException;
+import java.net.SocketException;
 import java.util.List;
 
 @Slf4j
@@ -20,7 +25,7 @@ import java.util.List;
 public class ErrorResponseService {
 
     public ErrorResponse fromException(Throwable e, ServerWebExchange exchange) {
-        var message = "default message";
+        var message = "An Unexpected error has occurred. Please try again later.";
         var eMessage = e.getMessage();
         var response = new ErrorResponse();
         var code = 500;
@@ -50,6 +55,16 @@ public class ErrorResponseService {
             String[] keys = StringUtils.substringsBetween(eMessage, "(", ")");
             String key = (keys != null) ? "'" + keys[0] : "";
             message = "Failed to read Http Message: cannot deserialize Enum instance " + key;
+        }
+        if (e instanceof ServerWebInputException) {
+            code = 400;
+            String[] keys = StringUtils.substringsBetween(eMessage, "(", ")");
+            String key = (keys != null) ? "'" + keys[0] : "";
+            message = "Failed to read Http Message: cannot deserialize Enum instance " + key;
+        }
+        if (e instanceof SocketException || e instanceof SSLException || e instanceof MismatchedInputException || e instanceof CannotGetMongoDbConnectionException) {
+            code = 503;
+            message = "Either remote server cannot be reached or network connection was reset/broken";
         }
         response.setStatus(code);
         response.setMessage(message);
