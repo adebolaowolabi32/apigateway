8390fff9e210046da9db7c5473aa812d90961ef4
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-08-07 16:53:57 +0100

    update to lookUp route method

--- src/main/java/com/interswitch/apigateway/handler/RouteHandlerMapping.java
+++ src/main/java/com/interswitch/apigateway/handler/RouteHandlerMapping.java
@@ -18,7 +18,6 @@ import java.net.URI;
 
 import static com.interswitch.apigateway.util.FilterUtil.decodeBearerToken;
 import static com.interswitch.apigateway.util.FilterUtil.getClaimAsStringFromBearerToken;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_PREDICATE_ROUTE_ATTR;
 
 @Component
 public class RouteHandlerMapping extends RoutePredicateHandlerMapping {
@@ -32,39 +31,30 @@ public class RouteHandlerMapping extends RoutePredicateHandlerMapping {
     }
 
     @Override
-    protected Mono<Route> lookupRoute(ServerWebExchange exchange) {
+    public Mono<Route> lookupRoute(ServerWebExchange exchange) {
         HttpHeaders headers = exchange.getRequest().getHeaders();
         JWT token = decodeBearerToken(headers);
         String environment = (token != null) ? getClaimAsStringFromBearerToken(token, "env") : "";
-        return this.routeLocator.getRoutes()
-                .concatMap(route -> Mono.just(route).filterWhen(r -> {
-                    exchange.getAttributes().put(GATEWAY_PREDICATE_ROUTE_ATTR, r.getId());
-                    return r.getPredicate().apply(exchange);
-                })
-                        .doOnError(e -> logger.error(
-                                "Error applying predicate for route: " + route.getId(),
-                                e))
-                        .onErrorResume(e -> Mono.empty()))
-                .next()
-                .flatMap(route -> {
-                    return repository.findByRouteId(route.getId())
-                            .flatMap(config -> {
-                                if (environment.equalsIgnoreCase("test") || environment.equalsIgnoreCase("sandbox")) {
-                                    URI sandbox = (config.getSandbox() != null) ? URI.create(config.getSandbox()) : route.getUri();
-                                    return Mono.just(Route.async().id(route.getId()).uri(sandbox).order(0).asyncPredicate(route.getPredicate())
-                                            .build());
-                                }
-                                if (environment.equalsIgnoreCase("uat") || environment.equalsIgnoreCase("dev")) {
-                                    URI uat = (config.getUat() != null) ? URI.create(config.getUat()) : route.getUri();
-                                    return Mono.just(Route.async().id(route.getId()).uri(uat).order(0).asyncPredicate(route.getPredicate())
-                                            .build());
-                                }
-                                if (logger.isDebugEnabled()) {
-                                    logger.debug("Route matched: " + route.getId());
-                                }
-                                return Mono.just(route);
-                            }).switchIfEmpty(Mono.error(new NotFoundException("Route Environment configuration not found")));
-                });
+        Mono<Route> lookupRoute = super.lookupRoute(exchange);
+        return lookupRoute.flatMap(route -> {
+            return repository.findByRouteId(route.getId())
+                    .flatMap(config -> {
+                        if (environment.equalsIgnoreCase("test") || environment.equalsIgnoreCase("sandbox")) {
+                            URI sandbox = (config.getSandbox() != null) ? URI.create(config.getSandbox()) : route.getUri();
+                            return Mono.just(Route.async().id(route.getId()).uri(sandbox).order(0).asyncPredicate(route.getPredicate())
+                                    .build());
+                        }
+                        if (environment.equalsIgnoreCase("uat") || environment.equalsIgnoreCase("dev")) {
+                            URI uat = (config.getUat() != null) ? URI.create(config.getUat()) : route.getUri();
+                            return Mono.just(Route.async().id(route.getId()).uri(uat).order(0).asyncPredicate(route.getPredicate())
+                                    .build());
+                        }
+                        if (logger.isDebugEnabled()) {
+                            logger.debug("Route matched: " + route.getId());
+                        }
+                        return Mono.just(route);
+                    }).switchIfEmpty(Mono.error(new NotFoundException("Route Environment configuration not found")));
+        });
 
     }
 }
