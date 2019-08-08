28f0c4a82a48be0958877fb981fd9037d5165347
Laura.Okpara <laura.okpara@interswitchgroup.com>
2019-08-08 14:09:52 +0100

    updated controller, repository

--- src/main/java/com/interswitch/apigateway/controller/EnvironmentController.java
+++ src/main/java/com/interswitch/apigateway/controller/EnvironmentController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/environment")
+@RequestMapping("/env")
 public class EnvironmentController {
 
     private MongoEnvRepository repository;
