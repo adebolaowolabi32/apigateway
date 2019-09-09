de487c6cafe198ca14f504328ecfd00bf1316a7a
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-09-09 14:36:27 +0100

    fixed passport service bug

--- src/test/java/com/interswitch/apigateway/DefaultSystemUserTests.java
+++ src/test/java/com/interswitch/apigateway/DefaultSystemUserTests.java
@@ -18,7 +18,7 @@ public class DefaultSystemUserTests {
 
     @Test
     public void testForDefaultSystemUser(){
-        StepVerifier.create(mongoUserRepository.findByUsername("adesegun.adeyemo")).assertNext(u -> {
+        StepVerifier.create(mongoUserRepository.findByUsername("api.gateway@interswitch.com")).assertNext(u -> {
             assertThat(u.getId()).isEqualTo("systemAdmin");
             assertThat(u.getRole()).isEqualTo(User.Role.ADMIN);
         }).expectComplete().verify();
