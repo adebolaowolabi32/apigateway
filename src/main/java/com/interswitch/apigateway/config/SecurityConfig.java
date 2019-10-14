be7094d7c91ecfe22dc78b9e0eaa6fecae4da6f3
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2019-10-14 10:18:13 +0100

    security implementation to exclude sign up and log in endpoints

--- src/main/java/com/interswitch/apigateway/config/SecurityConfig.java
+++ src/main/java/com/interswitch/apigateway/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.interswitch.apigateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -13,20 +15,29 @@ import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Value("${spring.security.oauth2.resourceserver.jwt.keyValue}")
     private String key;
 
+    private static List<String> noAuthEndpoints = Arrays.asList("/passport/oauth.*", "/actuator/prometheus/?", "/actuator/health/?", ".*/login/?", ".*/logout/?", ".*/register/?", ".*/signup/?", ".*/signin/?", ".*/signout/?", ".*/index/?", ".*/home/?", ".*/oauth/token/?", ".*/oauth/authenticate/?", ".*/oauth/authorize/?");
+
+
+
     @Bean
     public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange()
-                .pathMatchers("/passport", "/passport/oauth/**", "/actuator/prometheus", "**/actuator/health", "/actuator/health", "**/login", "**/logout", "**/register", "**/signup", "**/index", "**/home").permitAll()
                 .pathMatchers(HttpMethod.OPTIONS).permitAll()
-                .anyExchange().authenticated()
-                .and().csrf().disable()
+                .anyExchange().access((authenticationMono, context) -> {
+            String requestPath = context.getExchange().getRequest().getPath().toString().toLowerCase();
+            if (noAuthEndpoints.stream().anyMatch(endpoint -> requestPath.matches(endpoint)))
+                return Mono.just(new AuthorizationDecision(true));
+            return authenticationMono.flatMap(authentication -> Mono.just(new AuthorizationDecision(authentication.isAuthenticated())));
+        }).and().csrf().disable()
                 .oauth2ResourceServer()
                 .jwt().publicKey((RSAPublicKey) publicKey());
         return http.build();
